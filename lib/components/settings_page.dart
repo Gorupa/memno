e5c42cd5da98d94ca7c2d84cc7fc1801b0b10961
jydv402 <jydv402@gmail.com>
2025-06-16 11:05:44 +0530

    About section

--- lib/components/settings_page.dart
+++ lib/components/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:memno/theme/app_colors.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -18,22 +19,9 @@ class SettingsPage extends StatelessWidget {
           ),
           body: ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(32, 16, 0, 16),
-                child: Text("Settings",
-                    style: TextStyle(
-                        fontFamily: 'Product',
-                        fontSize: 28,
-                        color: colors.textClr)),
-              ),
-              Container(
-                height: 100,
-                padding: const EdgeInsets.all(16),
-                margin: const EdgeInsets.fromLTRB(2, 4, 2, 4),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50), color: colors.box),
-                child: SwitchListTile(
+              settingsTitle("Settings", colors),
+              settingsContainer(
+                SwitchListTile(
                   title: Text("Dark Mode",
                       style: TextStyle(
                           fontFamily: 'Product',
@@ -44,9 +32,60 @@ class SettingsPage extends StatelessWidget {
                     await colors.toggleTheme();
                   },
                 ),
+                colors,
               ),
+              settingsTitle("About", colors),
+              settingsContainer(
+                  ListTile(
+                      onTap: () {
+                        launchUrl(
+                            Uri.parse("https://github.com/jydv402/memno"));
+                      },
+                      trailing: Icon(Icons.arrow_outward_rounded,
+                          color: colors.textClr),
+                      title: Text(
+                        "Find us on GitHub",
+                        style: TextStyle(
+                            fontFamily: 'Product',
+                            fontSize: 18,
+                            color: colors.textClr),
+                      )),
+                  colors),
+              settingsContainer(
+                  ListTile(
+                      trailing:
+                          Icon(Icons.download_rounded, color: colors.textClr),
+                      title: Text(
+                        "Check for updates",
+                        style: TextStyle(
+                            fontFamily: 'Product',
+                            fontSize: 18,
+                            color: colors.textClr),
+                      )),
+                  colors)
             ],
           )),
+    );
+  }
+
+  Padding settingsTitle(String title, AppColors colors) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(32, 16, 0, 16),
+      child: Text(title,
+          style: TextStyle(
+              fontFamily: 'Product', fontSize: 28, color: colors.textClr)),
+    );
+  }
+
+  Container settingsContainer(Widget child, AppColors colors) {
+    return Container(
+      height: 100,
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.fromLTRB(2, 4, 2, 4),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50), color: colors.box),
+      child: child,
     );
   }
 }
